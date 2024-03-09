c81b03c63645fbf697be1b83c2332840c242c6ce
Mohamed Elhadouchi <melhadouchi387@gmail.com>
2024-03-09 16:14:57 +0100

    Finish ex00

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -75,30 +75,20 @@ void BitcoinExchange::parseUserInput(void) {
   std::string line;
 
   while (std::getline(data_stream, line)) {
-    // should find a better solotion for this trim
     line = trim(line);
-
-    if (!validLine(line)) {
-      printErr("\tNot valid lin");
-      // throw runtime_error("Error: not valid line");
-    } else {
-      this->getRate(this->date, this->value);
+    try {
+      if (!validLine(line)) {
+        printErr("Error: bad input => " + date);
+      } else {
+        this->getRate(this->date, this->value);
+      }
+    } catch (std::exception &e) {
+      std::cerr << e.what() << std::endl;
     }
-    // std::cout << "date ->> " << this->date << " value of ->> " << this->value
-    //           << std::endl;
   }
-  /* Check for valid format "|"*/
 }
 
 std::string BitcoinExchange::getRate(std::string const &date, float price) {
-  // date type => '2012-11-02'
-  // look for date in the db, if not found look for nearby -1,
-  // after that return the price on bined with the date.
-  // calculate the rate of the btc needed, and contruct a proper string to be
-  // returned.
-  // else if date was not found throw of date not in range.
-  // std::cout << "date ->> " << date << " value of ->> " << price << std::endl;
-
   BitcoinExchange::iterator it = this->btcDatabase.lower_bound(date);
 
   if (it == this->btcDatabase.end())
@@ -106,8 +96,8 @@ std::string BitcoinExchange::getRate(std::string const &date, float price) {
   if (it->first != date && it != this->btcDatabase.begin())
     it--;
 
-  std::cout << "date-> '" << it->first << "', price -> '" << it->second * price
-            << "' ." << std::endl;
+  std::cout << date << " => " << price << " = " << price * it->second << " ."
+            << std::endl;
 
   return "";
 }
@@ -150,24 +140,11 @@ bool BitcoinExchange::validLine(std::string &userInputLine) {
   date = trim(date);   // space
   price = trim(price); // space
 
-  // std::cout << "Line after trim => '" << userInputLine << "' ." << std::endl;
-  // std::cout << "date => '" << date << "' ." << std::endl;
-  // std::cout << "price => '" << price << "' ." << std::endl;
-  // std::cout << "=============================" << std::endl;
-
-  std::cout << "=============================" << std::endl;
   if (!validDate(date)) {
-    printErr("Not a valid date");
+    throw std::runtime_error("Error: bad input =>" + date);
+  } else if (!validValue(price)) {
     return false;
   }
-  std::cout << "=============================" << std::endl;
-  if (!validValue(price)) {
-    printErr("Not a valid price");
-    return false;
-  }
-
-  // store the required price and date to acces them later
-
   return true;
 }
 
@@ -193,13 +170,8 @@ bool fileStream(std::string const &fileName, std::string &bufferData) {
   ss << fs.rdbuf();      // reading from file
   bufferData = ss.str(); // writing data to buffer
   fs.close();
-
-  /* TODO: check first line is there and number of colomns */
   std::string firstLine;
   std::getline(ss, firstLine);
-  // type = 2. check for db file
-  // type = 1. check for input file
-  // std::cout << fileName << std::endl;
   if (!fileName.compare("data.csv")) {
     size_t count = std::count(firstLine.begin(), firstLine.end(), ',');
     // std::cout << count << " of ," << std::endl;
@@ -223,66 +195,44 @@ void printErr(std::string const &err) { std::cerr << err << std::endl; }
 
 /* validate date and price functions */
 bool BitcoinExchange::validDate(std::string &date) {
-  // check the format of the date is valid YYYY-MM-DD YYYY-MM-DD
   std::stringstream ss(date);
-
   std::string year;
   std::getline(ss, year, '-');
   std::string month;
   std::getline(ss, month, '-');
   std::string day;
   std::getline(ss, day, '-');
-
-  /* first check if all element are full. and in the sepcifque length*/
   if (year.empty() || month.empty() || day.empty())
     return false;
   if (year.length() != 4 || month.length() != 2 || day.length() != 2)
     return false;
-
-  /* validate the date in numbers*/
   if (!isStrNumber(year) || !isStrNumber(month) || !isStrNumber(day))
     return false;
-
-  // as a string. check day if its valid based on the given month
   size_t nYear = atol(year.c_str());
   size_t nMonth = atol(month.c_str());
   size_t nDay = atol(day.c_str());
-
-  /* check range of year and day and month*/
-
-  /* need to validate day based on the month 2 .
-   * and the year if it accepts / on
-   * 4 */
   if (!checkDate(nYear, nMonth, nDay))
     return false;
-
-  // std::cout << "year => '" << year << "' month => '" << month << "' day -> '"
-  //           << day << "' ." << std::endl;
-
-  // store the date as a string
   this->date = year + "-" + month + "-" + day;
 
   return true;
 }
 
 bool BitcoinExchange::validValue(std::string &value) {
+  if (!isStrNumber(value))
+    throw std::runtime_error("Error: value is not valid");
   float nValue = atof(value.c_str()); // converting str to int
-  //
-  if (nValue <= 0 || nValue > 1000)
-    return false;
-
-  // std::cout << "As intergers -< price => '" << nValue << "' str -> price ->
-  // '"
-  //           << value << "' ." << std::endl;
-
-  // storing the value
+  if (nValue <= 0)
+    throw std::runtime_error("Error: not a positive number");
+  if (nValue > 1000)
+    throw std::runtime_error("Error: too large a number");
   this->value = nValue;
   return true;
 }
 
 bool isStrNumber(std::string str) {
   for (size_t i = 0; i < str.length(); i++)
-    if (!isdigit(str.at(i)))
+    if (isalpha(str.at(i)))
       return false;
   return true;
 }
@@ -292,20 +242,12 @@ bool checkDate(size_t year, size_t month, size_t day) {
     return false;
   if (day < 1 || day > 31)
     return false;
-
-  // Handle February month
-  // with leap year
   if (month == 2) {
     if (isLeap(year))
       return (day <= 29);
     else
       return (day <= 28);
   }
-
-  // Months of April, June,
-  // Sept and Nov must have
-  // number of days less than
-  // or equal to 30.
   if (month == 4 || month == 6 || month == 9 || month == 11)
     return (day <= 30);
 
@@ -313,9 +255,5 @@ bool checkDate(size_t year, size_t month, size_t day) {
 }
 
 bool isLeap(int year) {
-  // Return true if year
-  // is a multiple of 4 and
-  // not multiple of 100.
-  // OR year is multiple of 400.
   return (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0));
 }
